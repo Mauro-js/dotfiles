568cd2cb3876fe28e0a3578c466fff7801d836b9
Mauro-js <niev.mauro@gmail.com>
2024-06-14 18:34:40 -0300

    pequeños cambios

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -8,10 +8,10 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
 polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar bottom 2>&1 | tee -a /tmp/bottom.log & disown
+#polybar bottom 2>&1 | tee -a /tmp/bottom.log & disown
 
 # Check if HDMI-A-0 is connected
 if xrandr | grep -q 'HDMI-A-0 connected'; then
     polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
-    polybar bottom2 2>&1 | tee -a /tmp/bottom2.log & disown
+    #polybar bottom2 2>&1 | tee -a /tmp/bottom2.log & disown
 fi
